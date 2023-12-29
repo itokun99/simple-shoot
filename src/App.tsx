a023dd0a620428b0d55652f495a06f191e6e8b94
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2023-12-29 14:26:54 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { useEffect } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+
+function DetailPage() {
+  return (
+    <div>
+      <Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="ESOP Stockbit" />
+        <meta property="og:description" content="ESOP Stockbit" />
+        <meta
+          property="og:image"
+          content="%PUBLIC_URL%/stockbit-logo-dark.svg"
+        />
+      </Helmet>
+      <h1>Test Detail Page</h1>
+    </div>
+  );
+}
+
+function HomePage() {
+  return <div>Home page</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div style={{ display: "flex", gap: 2 }}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/detail">Detail</NavLink>
+        </div>
+        <Routes>
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { hydrate, render } from "react-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+//
+const helmetContext = {};
+const App1 = () => (
   <React.StrictMode>
+      <HelmetProvider context={helmetContext}>
     <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
+
+
+// const html = renderToString(app);
+
+// const { helmet } = helmetContext;
+
+//
+const root = document.getElementById("root");
+
+if (root?.hasChildNodes()) {
+  hydrate(<App1 />, root);
+} else {
+  render(<App1 />, root);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
